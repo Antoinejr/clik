92032c7b39ea3692bdddbf39f46aba55e6aff23f
s2261065 <a.m.irokosu@student.utwente.nl>
2022-08-16 23:45:05 +0200

    built a passable ui for the program

--- clik-project/App.js
+++ clik-project/App.js
@@ -1,20 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './components/WelcomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <WelcomeScreen/>;}
